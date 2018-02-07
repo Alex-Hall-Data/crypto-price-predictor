a7e82ceed9fd6c665224d5e0e0405c6d1835f0fa
alex hall <alex.hall@outplay.com>
2018-02-07 17:18:41 +0000

    Data processing and basic RNN added

--- RNN-script.py
+++ RNN-script.py
@@ -11,13 +11,14 @@ import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn import model_selection
+from random import shuffle
 
 
 
 # ### The Data
 
 # In[2]:
-raw_data = pd.read_csv("C:\\Users\\Alex\\Documents\\Python\\crypto-predicter\\crypto-markets.csv")
+raw_data = pd.read_csv("C:\\Users\\alex.hall\\Documents\\_git\\crypto-price-predictor\\crypto-markets.csv")
 
 btc_data = raw_data[raw_data['symbol']=="BTC"]
 
@@ -34,62 +35,74 @@ close_price_data=btc_data['close']
 # Num of steps in batch (also used for prediction steps into the future)
 num_time_steps = 14
 
-#split into batches of 'num_time_Steps' days
+#split into batches of 'num_time_Steps +1' days (the last element will be the y_true for the relevant batch)
 data_batches=list() #list of 2 week batches to be used as train/test set
 i=0
-while (i < len(close_price_data)-num_time_steps):
-    data_batches.append((close_price_data.ix[i:(i+num_time_steps-1)]))
+while (i < len(close_price_data)-num_time_steps-1):
+    data_batches.append((close_price_data.ix[i:(i+num_time_steps)]))
     i=i+1
+
+data_batches=np.asarray(data_batches)
+data_batches=np.random.permutation(data_batches) # shuffle array
+
 
 x_train,x_test = model_selection.train_test_split(data_batches,test_size=0.2)
 
+#keep unscaled sets for now (may be useful later)
+x_train_unscaled=x_train
+x_test_unscaled=x_test
 
 #scale each batch relative to max value
+def scale_batch(batch):
+    batch_max=max(batch)
+    scaled_batch=list()
+    for i in range(0,len(batch)):
+        scaled_batch.append(batch[i]/batch_max)
+    scaled_batch=np.asarray(scaled_batch)
+    return scaled_batch
+        
 
 
-
+for i in range(0,len(x_train)):
+    x_train[i]=scale_batch(x_train[i])
+    
+for i in range(0,len(x_test)):
+    x_test[i]=scale_batch(x_test[i])
 
 
 # In[6]:
+#populate y_true arrays
+
+y_train=np.empty(shape=(len(x_train)))
+for i in range(0,len(x_train)):
+    y_train[i]=x_train[i][num_time_steps]
+    
+y_test=np.empty(shape=(len(x_test)))
+for i in range(0,len(x_test)):
+    y_test[i]=x_test[i][num_time_steps]
 
 
-y1,y2,ts = ts_data.next_batch(1,num_time_steps,True)
-
-
-# In[7]:
-
-
-plt.plot(ts.flatten()[1:],y2.flatten(),'*')
+#remove the y_true values from the input data
+X_train=np.empty(shape=(len(x_train),num_time_steps))
+X_test=np.empty(shape=(len(x_test),num_time_steps))
+for i in range(0,len(x_train)):
+    X_train[i]=np.delete(x_train[i],[len(x_train[i])-1])
+    
+y_test=np.empty(shape=(len(x_test)))
+for i in range(0,len(x_test)):
+    X_test[i]=np.delete(x_test[i],[len(x_test[i])-1])
 
 
 # In[8]:
+#pick a random training instance and plot it
+
+train_inst = x_train[np.random.randint(len(X_train))]
 
 
-plt.plot(ts_data.x_data,ts_data.y_true,label='Sin(t)')
-plt.plot(ts.flatten()[1:],y2.flatten(),'*',label='Single Training Instance')
-plt.legend()
+plt.plot(list(range(0,num_time_steps+1)),train_inst)
+plt.title("random training instance")
 plt.tight_layout()
 
-
-# ### A Training Instance and what to predict
-# 
-# We are trying to predict a time series shifted over by t+1
-
-# In[9]:
-
-
-train_inst = np.linspace(5,5 + ts_data.resolution * (num_time_steps + 1), num_time_steps+1)
-
-
-# In[10]:
-
-
-plt.title("A training instance", fontsize=14)
-plt.plot(train_inst[:-1], ts_data.ret_true(train_inst[:-1]), "bo", markersize=15,alpha=0.5 ,label="instance")
-plt.plot(train_inst[1:], ts_data.ret_true(train_inst[1:]), "ko", markersize=7, label="target")
-
-
-# ___________
 
 # # Creating the Model
 
@@ -113,7 +126,7 @@ num_outputs = 1
 # learning rate, 0.0001 default, but you can play with this
 learning_rate = 0.0001
 # how many iterations to go through (training steps), you can play with this
-num_train_iterations = 2000
+num_train_iterations = len(X_train)
 # Size of the batch of data
 batch_size = 1
 
@@ -123,8 +136,8 @@ batch_size = 1
 # In[13]:
 
 
-X = tf.placeholder(tf.float32, [None, num_time_steps, num_inputs])
-y = tf.placeholder(tf.float32, [None, num_time_steps, num_outputs])
+X = tf.placeholder(tf.float32, [None,num_time_steps,num_inputs])
+y = tf.placeholder(tf.float32, [None,1,num_outputs])
 
 
 # ____
@@ -211,7 +224,7 @@ init = tf.global_variables_initializer()
 
 # ONLY FOR GPU USERS:
 # https://stackoverflow.com/questions/34199233/how-to-prevent-tensorflow-from-allocating-the-totality-of-a-gpu-memory
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.75)
+#gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.75)
 
 
 # In[25]:
@@ -223,21 +236,30 @@ saver = tf.train.Saver()
 # In[26]:
 
 
-with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+with tf.Session() as sess:
     sess.run(init)
     
     for iteration in range(num_train_iterations):
         
-        X_batch, y_batch = ts_data.next_batch(batch_size, num_time_steps)
+        X_batch = np.reshape(X_train[iteration],(batch_size,num_time_steps,num_inputs))
+        y_batch = np.reshape(y_train[iteration],(batch_size,1,num_outputs))
         sess.run(train, feed_dict={X: X_batch, y: y_batch})
         
-        if iteration % 100 == 0:
+        if iteration % 10 == 0:
             
             mse = loss.eval(feed_dict={X: X_batch, y: y_batch})
             print(iteration, "\tMSE:", mse)
     
     # Save Model for Later
     saver.save(sess, "./rnn_time_series_model")
+
+
+
+
+
+
+
+
 
 
 # ### Predicting a time series t+1
