ab9981514b5b7e0da28b6d494fbbfdbfaea3df8d
Alex Hall <northern.bantam@gmail.com>
2018-02-08 09:04:27 +0000

    experimented with optimiser

--- RNN-script.py
+++ RNN-script.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 # # TODO:
+#need a custom loss function to maximise profit
 #the graphs at the end look prett but remember the only number we are interested in is the final day - only use this as th basis for decisions
 #for each test set element, record the actual and predicted % change
 #start hyperparameter tuning
@@ -54,7 +55,7 @@ data_batches=np.asarray(data_batches)
 
 i=1
 while (i < len(close_price_data)-num_time_steps):
-    data_batches_shifted.append((close_price_data[i:(i+num_time_steps)]))
+    data_batches_shifted.append((close_price_data[i+num_time_steps]))###
     i=i+1
 
 data_batches_shifted=np.asarray(data_batches_shifted)
@@ -67,13 +68,19 @@ data_batches_unscaled=data_batches
 
 #scale each batch relative to max value
 def scale_batch(batch,shifted_batch):
-    batch_max=max(np.concatenate([batch,shifted_batch]))
+    batch_max=max(np.append(batch,shifted_batch))
     scaled_batch=list()
     for i in range(0,len(batch)):
         scaled_batch.append(batch[i]/batch_max)
     scaled_batch=np.asarray(scaled_batch)
     return scaled_batch
-        
+
+#as above but scales the true values
+def scale_true(shifted_batch,batch):
+    batch_max=max(np.append(batch,shifted_batch))
+    scaled_y=shifted_batch/batch_max
+    return scaled_y
+    
 
 data_batches_normalised=np.empty(shape=np.shape(data_batches))
 for i in range(0,len(data_batches)):
@@ -81,15 +88,10 @@ for i in range(0,len(data_batches)):
     
 data_batches_shifted_normalised=np.empty(shape=np.shape(data_batches_shifted))
 for i in range(0,len(data_batches_shifted)):
-    data_batches_shifted_normalised[i]=scale_batch(data_batches_shifted[i],data_batches[i])
+    data_batches_shifted_normalised[i]=scale_true(data_batches_shifted[i],data_batches[i])
 
 
 
-
-
-
-
-#x_train,x_test = model_selection.train_test_split(data_batches,test_size=0.2)
 
 # In[6]:
 #MAKE TRAIN AND TEST SET 
@@ -147,7 +149,7 @@ batch_size = 1
 
 
 X = tf.placeholder(tf.float32, [None,num_time_steps,num_inputs])
-y = tf.placeholder(tf.float32, [None,num_time_steps,num_outputs])
+y = tf.placeholder(tf.float32, [None,1,num_outputs])
 
 
 # ____
@@ -167,9 +169,9 @@ y = tf.placeholder(tf.float32, [None,num_time_steps,num_outputs])
 # In[15]:
 
 
-#cell = tf.contrib.rnn.OutputProjectionWrapper(
-#    tf.contrib.rnn.BasicLSTMCell(num_units=num_neurons, activation=tf.nn.relu),
-#    output_size=num_outputs)
+cell = tf.contrib.rnn.OutputProjectionWrapper(
+    tf.contrib.rnn.BasicLSTMCell(num_units=num_neurons, activation=tf.nn.relu),
+    output_size=num_outputs)#num_outputs
 
 
 # In[16]:
@@ -191,11 +193,11 @@ y = tf.placeholder(tf.float32, [None,num_time_steps,num_outputs])
 # In[18]:
 
 
-n_neurons = 100
-n_layers = 3
+#n_neurons = 100
+#n_layers = 3
 
-cell = tf.contrib.rnn.OutputProjectionWrapper(tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(num_units=n_neurons)
-          for layer in range(n_layers)]),output_size=num_outputs)
+#cell = tf.contrib.rnn.OutputProjectionWrapper(tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(num_units=n_neurons)
+#          for layer in range(n_layers)]),output_size=num_outputs)
 
 
 # _____
@@ -214,7 +216,7 @@ outputs, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
 # In[20]:
 
 
-loss = tf.sqrt(tf.reduce_mean(tf.square(outputs[0][num_time_steps-1][0] - y[0][num_time_steps-1][0]))) # RMSE - minimised for the last day of the series (ie the unknown one)
+loss = tf.sqrt(tf.reduce_mean(tf.square(outputs[0][num_time_steps-1][0] - y[0]))) # RMSE - minimised for the last day of the series (ie the unknown one)
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train = optimizer.minimize(loss)
 
@@ -252,7 +254,7 @@ with tf.Session() as sess:
     for iteration in range(num_train_iterations):
         
         X_batch = np.reshape(x_train[iteration],(batch_size,num_time_steps,num_inputs))
-        y_batch = np.reshape(y_train[iteration],(batch_size,num_time_steps,num_outputs))
+        y_batch = np.reshape(y_train[iteration],(batch_size,1,num_outputs))
         sess.run(train, feed_dict={X: X_batch, y: y_batch})
         
         if iteration % 10 == 0:
@@ -261,7 +263,7 @@ with tf.Session() as sess:
             print(iteration, "\tRMSE:", rmse)
             
             #accuracy on test set
-            test_rmse=loss.eval(feed_dict={X: np.reshape(x_test,( np.shape(x_test)[0],np.shape(x_test)[1],1)),y:np.reshape(y_test,( np.shape(y_test)[0],np.shape(y_test)[1],1))})
+            test_rmse=loss.eval(feed_dict={X: np.reshape(x_test,( np.shape(x_test)[0],np.shape(x_test)[1],1)),y:np.reshape(y_test,( np.shape(y_test)[0],1,1))})
             print(iteration, "\tRMSE on test:", test_rmse)
     
     # Save Model for Later
@@ -281,7 +283,7 @@ with tf.Session() as sess:
     
     random_selection=randint(0,len(x_test))
     X_new = np.reshape(x_test[random_selection],(1,num_time_steps,num_inputs))
-    y_true = np.reshape(y_test[random_selection],(1,num_time_steps,1))
+    y_true = np.reshape(y_test[random_selection],(1,1,1))
     y_pred = sess.run(outputs, feed_dict={X: X_new})
 
 
@@ -292,10 +294,10 @@ plt.title("Testing Example")
 
 # Test Instance
 plt.plot(list(range(0,num_time_steps)),X_new[0],label="Input")
-plt.plot(list(range(1,num_time_steps+1)), y_true[0],label="Actual")
+plt.plot(num_time_steps, y_true[0],'ro',label="Actual")
 
 # Target to Predict
-plt.plot(list(range(1,num_time_steps+1)), y_pred[0], label="Predicted")
+plt.plot(list(range(1,num_time_steps+1)), y_pred[0],'bs', label="Predicted")
 
 
 plt.xlabel("Time")
@@ -303,7 +305,7 @@ plt.legend()
 plt.tight_layout()
 
 axes = plt.gca()
-axes.set_ylim([min(y_true[0])[0],1])
+axes.set_ylim([min(X_new[0])[0],1])
 
 
 
